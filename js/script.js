ca720b94dd5a4e3b97602791abaac78dfda543c3
Roman Lema <roman.teka1@yahoo.com>
2022-02-23 21:13:41 -0500

    added script

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+// console.log("hello");//single line comment
+console.log("Roman");
+console.log("my faverite-food is ketfo")
+
+// This is the first 
+// script-practice.   // multi line comment
