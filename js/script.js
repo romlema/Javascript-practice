6a4a7e2b6087214f0f0a23b6dfa9acce35690a04
Roman Lema <roman.teka1@yahoo.com>
2022-03-01 18:08:59 -0500

    BigInt length propertyadd

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
 
 // console.log("hello");//single line comment
-console.log("Roman");
-console.log("my faverite-food is ketfo");
+// console.log("Roman");
+// console.log("my faverite-food is ketfo");
 
 // This is the first 
 // script-practice.   // multi line comment
+
+
+// Day-2 Daily Challenge
+
+var word="supercalifragilisticexpialidocious";
+console.log(word.length);
+// console.log anynumber
+console.log(12346778999876654433339n);
+// Console log a BigInt
+console.log(1024n.toString());
+// expected output: "1024"
+
+//Use typeof to confirm your number and BigInt values
+
+ console.log(34n);//34n
+ console.log(BigInt(34n));//34n
